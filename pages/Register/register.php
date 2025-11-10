8fd593a396ea51e4fda25b0ea4fd3e1a3ba38fe5
Huhucoching <dongcoching@gmail.com>
2025-11-11 00:40:33 +0800

    dpa tapos

--- pages/Register/register.php
+++ pages/Register/register.php
@@ -8,8 +8,11 @@ $conn = mysqli_connect($servername, $username,$password, $dbname);
 
 if (!$conn){
 die ("Connection Failed: " . mysqli_connect_error());
-}
-    $action = $_POST['signin'];
+           }
+
+
+    $action = $_POST['action'];
+    $idregistration  = $_POST['idregistration'];
     $username = $_POST['username'];
     $password = $_POST['password'];
     $address = $_POST['address'];
@@ -20,23 +23,21 @@ die ("Connection Failed: " . mysqli_connect_error());
     $number = $_POST['number'];
 
     if(!empty($username) && !empty($fullname) && !empty($address) && !empty($gmail) && !empty($zipcode) && !empty($contact) && !empty($birthday) && !empty($password)){
-    if($action == "ADD"){     
-            $sql = "insert into users(username, gmail, password, address, fullname, zipcode, birthday, number) VALUES ('$username', '$gmail', '$password', '$address', '$fullname', '$zipcode', '$birthday', '$number')";
+    if($action == "ADD USER"){     
+            $sql = "INSERT INTO registration(username, gmail, password, address, fullname, zipcode, birthday, number) 
+            VALUES ('$username', '$gmail', '$password', '$address', '$fullname', '$zipcode', '$birthday', '$number')";
             $msg = "Registration successful!";             
             }  
            if(mysqli_query( $conn, $sql))
-                            {
-
-                       echo $msg;         
-                            }  
+            {
+            echo $msg;         
+            }  
              else   
-                    { 
-                  
+            { 
         echo "Error ". $sql . "<br>";
         mysqli_error($conn);
-             }
-        
             }
-   echo "<br><br><br><a a href='home.html'> Back to signUp List</a>"
+            }
+   echo "<br><br><br><a a href='signUp.php'> Back to signUp List</a>"
 
 ?>
